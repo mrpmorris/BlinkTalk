0e2ba583512767e86689e801eba24277e26726b4
Peter <mrpmorris@gmail.com>
2018-07-11 22:43:24 +0100

    Phrases

--- Assets/Scenes/Typing/Scripts/PhraseService.cs
+++ Assets/Scenes/Typing/Scripts/PhraseService.cs
@@ -1,83 +1,114 @@
 ﻿using BlinkTalk.Typing.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace BlinkTalk.Typing
 {
-
     public static class PhraseService
     {
-        public static List<string> GetWordSuggestions(IEnumerable<int> wordIds, string currentWord, int numberOfWords)
-        {
-            var conditions = new List<string>();
-            wordIds = wordIds.Reverse().Take(3).Reverse();
-            int wordNumber = 1;
-            foreach (int wordId in wordIds)
-            {
-                conditions.Add($"WordSequences.Word{wordNumber}Id = {wordId}");
-                wordNumber++;
-            }
-            if (!string.IsNullOrEmpty(currentWord))
-                conditions.Add($"Words.Word like '{currentWord}%'");
-
-            string sqlConditions = string.Join(" and ", conditions);
-            if (!string.IsNullOrEmpty(sqlConditions))
-                sqlConditions = "where " + sqlConditions;
-
-            string sql = $"" +
-                $"Select distinct Words.Word from WordSequences join Words on Words.Id = WordSequences.Word{wordNumber}Id" +
-                $" {sqlConditions}" +
-                $" order by WordSequences.UsageCount desc, LastUsedDate desc, Words.UserSelectionCount desc" +
-                $" limit {numberOfWords}";
-            DataTable data = PersistenceService.DB.ExecuteQuery(sql);
-            return data.Rows.Select(x => (string)x["Word"]).ToList();
-        }
-
         /// <summary>
         /// This method will run a 4 word sliding window across the sentence and update
         /// the database so we can learn which word sequences the user uses most often.
-        /// The entries will be 
-        /// Word1, null, null, null
-        /// Word1, Word2, null, null
-        /// Word1, Word2, Word3, null
-        /// Word1, Word2, Word3, Word4
-        /// Word2, Word3, Word4, Word5
-        /// etc
+        /// For a 5 word sentence the entries will be
+        /// null, null, null, word1
+        /// null, null, word1, word2
+        /// null, word1, word2, word3
+        /// word1, word2, word3, word4
+        /// word2, word3, word4, word5
         /// </summary>
         /// <param name="wordIds"></param>
         public static void IncrementPhraseUsage(IEnumerable<int> wordIds)
         {
-            var phraseWindow = new List<int>();
-            foreach(int wordId in wordIds)
-            {
-                phraseWindow.Add(wordId);
-                // Ensure a maximum of 4 words
-                if (phraseWindow.Count > 4)
-                    phraseWindow.RemoveAt(0);
+            IEnumerable<PhraseWindow> phraseWindows = PhraseWindow.CreatePhraseWindows(wordIds);
+            foreach (PhraseWindow phraseWindow in phraseWindows)
                 IncrementPhraseWindow(phraseWindow);
-            }
         }
 
-        private static void IncrementPhraseWindow(List<int> wordIds)
+        public static List<string> GetWordSuggestions(IEnumerable<int> wordIds, string currentWord, int numberOfWords)
         {
-            var conditions = new List<string>();
-            var sqlWordIds = new List<string>();
-            for(int wordIndex = 0; wordIndex < 4; wordIndex++)
-            {
-                int wordNumber = wordIndex + 1;
-                string wordIdValue = wordIndex < wordIds.Count ? wordIds[wordIndex].ToString() : "null";
-                conditions.Add($"Word{wordNumber}Id = {wordIdValue}");
-                sqlWordIds.Add(wordIdValue);
-            }
-            string sqlConditions = string.Join(" and ", conditions);
+            // First ensure there are at least 3 words
+            var nullableWordIds = new List<int?> { null, null, null };
+            nullableWordIds.AddRange(wordIds.Cast<int?>());
+            // Only take the last 3 values
+            nullableWordIds = nullableWordIds
+                .Select(x => x) // IEnumerable<string> instead of List<string>
+                .Reverse() // Otherwise .Reverse is a List<T> method and not a LINQ extension
+                .Take(3)
+                .Reverse()
+                .ToList();
+
+            string nullableWordIdsAsString = ConvertNullableIntsToCommaList(nullableWordIds);
+            string scoreSQL = CreateScoreSql(nullableWordIds);
+
+            string sqlConditions =
+                $" PrecedingWord3Id in ({nullableWordIdsAsString})" +
+                $" and PrecedingWord2Id in ({nullableWordIdsAsString})" +
+                $" and PrecedingWord1Id in ({nullableWordIdsAsString})";
+            if (!string.IsNullOrEmpty(currentWord))
+                sqlConditions += $" and Words.Word like '{currentWord}%'";
+
+            string sql = $"" +
+                $"Select {scoreSQL}, Words.Word from WordSequences join Words on Words.Id = WordSequences.SuggestedWordId" +
+                $" where {sqlConditions}" +
+                $" order by (Score1 + Score2 + Score3) desc, WordSequences.UsageCount desc, LastUsedDate desc, Words.UserSelectionCount desc";
+            DataTable data = PersistenceService.DB.ExecuteQuery(sql);
+            return data.Rows
+                .Select(x => (string)x["Word"])
+                .Distinct()
+                .Take(numberOfWords)
+                .ToList();
+        }
+
+        private static string ConvertNullableIntsToCommaList(IEnumerable<int?> nullableWordIds)
+        {
+            IEnumerable<string> stringValues = nullableWordIds
+                .Select(x => x.ToStringOrDefault());
+            return string.Join(",", stringValues);
+        }
+
+        private static string CreateScoreSql(List<int?> nullableWordIds)
+        {
+            if (nullableWordIds.Count != 3)
+                throw new ArgumentException("Must be 3 elements", nameof(nullableWordIds));
+
+            var calculatedColumns = new List<string>();
+            calculatedColumns.Add(CreateScoreSqlColumn(3, nullableWordIds, 3, 2, 1));
+            calculatedColumns.Add(CreateScoreSqlColumn(2, nullableWordIds, 2, 3, 2));
+            calculatedColumns.Add(CreateScoreSqlColumn(1, nullableWordIds, 1, 2, 3));
+            return string.Join(",", calculatedColumns);
+        }
+
+        private static string CreateScoreSqlColumn(int precedingWordNumber, List<int?> nullableWordIds, params int[] scores)
+        {
+            string columnName = $"PrecedingWord{precedingWordNumber}Id";
+            string result = $"case {columnName}" +
+                $" when {nullableWordIds[0].ToStringOrDefault()} then {scores[0]}" +
+                $" when {nullableWordIds[1].ToStringOrDefault()} then {scores[1]}" +
+                $" when {nullableWordIds[2].ToStringOrDefault()} then {scores[2]}" +
+                $" else 0 end Score{precedingWordNumber}";
+            return result;
+        }
+
+        private static void IncrementPhraseWindow(PhraseWindow phraseWindow)
+        {
+            string sqlConditions =
+                $"PrecedingWord3Id = {phraseWindow.PrecedingWord3Id.ToStringOrDefault()}" +
+                $" and PrecedingWord2Id = {phraseWindow.PrecedingWord2Id.ToStringOrDefault()}" +
+                $" and PrecedingWord1Id = {phraseWindow.PrecedingWord1Id.ToStringOrDefault()}" +
+                $" and SuggestedWordId = {phraseWindow.SuggestedWordId}";
 
             int today = PersistenceService.DB.TodayAsInt();
             string sql = $"Select ID from WordSequences where {sqlConditions} limit 1";
             DataTable data = PersistenceService.DB.ExecuteQuery(sql);
             if (data.Rows.Count == 0)
             {
-                sql = "Insert into WordSequences(Word1Id, Word2Id, Word3Id, Word4Id, UsageCount, LastUsedDate)" +
-                    $" values ({sqlWordIds[0]}, {sqlWordIds[1]}, {sqlWordIds[2]}, {sqlWordIds[3]}, 1, {today})";
+                sql = "Insert into WordSequences(PrecedingWord3Id, PrecedingWord2Id, PrecedingWord1Id, SuggestedWordId, UsageCount, LastUsedDate)" +
+                    $" values (" +
+                    $" {phraseWindow.PrecedingWord3Id.ToStringOrDefault()}," +
+                    $" {phraseWindow.PrecedingWord2Id.ToStringOrDefault()}," +
+                    $" {phraseWindow.PrecedingWord1Id.ToStringOrDefault()}," +
+                    $" {phraseWindow.SuggestedWordId}, 1, {today})";
             }
             else
             {
@@ -86,5 +117,7 @@ namespace BlinkTalk.Typing
             }
             PersistenceService.DB.ExecuteNonQuery(sql);
         }
+
+
     }
 }
---
+++ Assets/Scenes/Typing/Scripts/PhraseWindow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlinkTalk.Typing
+{
+    public class PhraseWindow
+    {
+        public readonly int? PrecedingWord3Id;
+        public readonly int? PrecedingWord2Id;
+        public readonly int? PrecedingWord1Id;
+        public readonly int SuggestedWordId;
+
+        public PhraseWindow(int wordId)
+        {
+            SuggestedWordId = wordId;
+        }
+
+        public PhraseWindow(int? precedingWord3Id, int? precedingWord2Id, int? precedingWord1Id, int suggestedWordId)
+        {
+            PrecedingWord3Id = precedingWord3Id;
+            PrecedingWord2Id = precedingWord2Id;
+            PrecedingWord1Id = precedingWord1Id;
+            SuggestedWordId = suggestedWordId;
+        }
+
+        public PhraseWindow Next(int wordId)
+        {
+            return new PhraseWindow(
+                precedingWord3Id: PrecedingWord2Id,
+                precedingWord2Id: PrecedingWord1Id,
+                precedingWord1Id: SuggestedWordId,
+                suggestedWordId: wordId);
+        }
+
+        public static IEnumerable<PhraseWindow> CreatePhraseWindows(IEnumerable<int> wordIds)
+        {
+            var result = new List<PhraseWindow>();
+            var suggestedWordId = wordIds.First();
+            wordIds = wordIds.Skip(1);
+            var wordWindow = new PhraseWindow(suggestedWordId);
+            result.Add(wordWindow);
+            foreach (int wordId in wordIds)
+            {
+                wordWindow = wordWindow.Next(wordId);
+                result.Add(wordWindow);
+            }
+            return result;
+        }
+    }
+}
